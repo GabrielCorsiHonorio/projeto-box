9ff717e83bf3f22785686cb08f30b3505af5e591
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-06-20 12:34:47 -0300

    teste apagar3

--- pages/api/executar.js
+++ pages/api/executar.js
@@ -8,6 +8,40 @@ let estadoGlobal = null;
 let execucaoGlobal = null;
 let idGlobal = null;
 
+async function apagarAcao (id){
+
+
+    const hoje = moment.tz('America/Sao_Paulo').format('YYYY-MM-DD');
+     const hojeFormat = moment.tz(hoje,'America/Sao_Paulo').toISOString();
+     console.log('Valor do dia no formato', hojeFormat);
+     console.log('Id que será apagado',id);
+
+   // Buscar a execução mais recente
+   const lastExecution = await prisma.executedAction.findFirst({
+     orderBy: { id: 'desc' },
+   });
+   console.log('Data da ultima execucao',lastExecution);
+
+   // Atualizar a data de execução
+   await prisma.executedAction.update({
+       where: { id: lastExecution.id },
+       data: { date: hojeFormat },
+     });
+     console.log('Date que está sendo salva', hojeFormat);
+
+   // Excluir a ação do banco de dados
+   await prisma.action.delete({
+     where: { id: parseInt(id) },
+   });
+
+   await prisma.$disconnect();
+
+   console.log('Ação original excluída:', id);
+   idGlobal = null;
+   res.status(201).json({ message: 'Ação executada e excluída com sucesso' });
+
+}
+
 export default async function handler(req, res) {
     try {
         const { action } = req.query;
@@ -69,37 +103,4 @@ export default async function handler(req, res) {
         console.error('Internal server error:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-}
-async function apagarAcao (id){
-
-
-       const hoje = moment.tz('America/Sao_Paulo').format('YYYY-MM-DD');
-        const hojeFormat = moment.tz(hoje,'America/Sao_Paulo').toISOString();
-        console.log('Valor do dia no formato', hojeFormat);
-        console.log('Id que será apagado',id);
-
-      // Buscar a execução mais recente
-      const lastExecution = await prisma.executedAction.findFirst({
-        orderBy: { id: 'desc' },
-      });
-      console.log('Data da ultima execucao',lastExecution);
-
-      // Atualizar a data de execução
-      await prisma.executedAction.update({
-          where: { id: lastExecution.id },
-          data: { date: hojeFormat },
-        });
-        console.log('Date que está sendo salva', hojeFormat);
-
-      // Excluir a ação do banco de dados
-      await prisma.action.delete({
-        where: { id: parseInt(id) },
-      });
-
-      await prisma.$disconnect();
-
-      console.log('Ação original excluída:', id);
-      idGlobal = null;
-      res.status(201).json({ message: 'Ação executada e excluída com sucesso' });
-
 }
